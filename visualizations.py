5281fd0682e61a6a49779e94b4201e8c9adb024d
S-Battle <sethbattle@gmail.com>
2025-05-31 13:45:13 -0700

    another commit

--- visualizations.py
+++ visualizations.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt 
-import matplotlib 
 matplotlib.use('Agg')
+import matplotlib 
+import matplotlib.pyplot as plt 
 import seaborn as sns 
 import plotly.express as px
 
